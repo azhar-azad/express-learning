c7a1c76a9e4aaa3845dce3326b91c6be3cfa0e83
azhar-azad <azhar.u.azad@gmail.com>
2020-12-20 20:23:49 +0600

    hr-api project dependencies added

---
+++ practice/hr-api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Hr_db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to MongoDB'));
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('HOME PAGE test');
+});
+
+app.listen(port, () => {
+  console.log(`App is running at http://localhost:${port}`);
+});
