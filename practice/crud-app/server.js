ca6735e757f070a91ce8ae0bd95f8d6f52b095fa
azhar-azad <azhar.u.azad@gmail.com>
2021-05-08 23:38:12 +0600

    crud-app: modules

--- practice/crud-app/server.js
+++ practice/crud-app/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 dotenv.config({path: 'config.env'});
 
@@ -9,6 +11,18 @@ const PORT = process.env.PORT || 8080;
 
 // log requests
 app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+// set view engine
+app.set('view engine', 'ejs');
+// app.set('views', path.resolve(__dirname, 'views/home'));
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/', (req, res) => {
   res.send('Crud Application');
