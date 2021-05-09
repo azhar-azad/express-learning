c751a21c326a885cbf1ba127802eccaf5738c55d
azhar-azad <azhar.u.azad@gmail.com>
2021-05-09 13:13:14 +0600

    crud-app: update endpoint done

--- practice/crud-app/server/controllers/controller.js
+++ practice/crud-app/server/controllers/controller.js
@@ -35,11 +35,30 @@ exports.create = (req, res) => {
 // retrieve and return all-user/single-user
 exports.find = (req, res) => {
   
+  Userdb.find().then(user => {
+    res.send(user);
+  }).catch(err => {
+    res.status(500).send({message: err.message || 'Error occurred while retrieving user information'});
+  });
 };
 
 // update a new identified user by user id
 exports.update = (req, res) => {
   
+  if (!req.body) {
+    return res.status(400).send({message: 'Data to update can not be empty'});
+  }
+
+  const id = req.params.id;
+  Userdb.findByIdAndUpdate(id, req.body, { useFindAndModify: false }).then(data => {
+    if (!data) {
+      res.status(404).send({message: `Cannot update user with ${id}. Maybe user not found!`});
+    } else {
+      res.send(data);
+    }
+  }).catch(err => {
+    res.status(500).send({message: 'Error updating user information!'});
+  })
 };
 
 // delete a user with specified user id
