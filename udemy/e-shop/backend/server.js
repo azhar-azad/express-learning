3b24d44f72ff21b89288776bdaa986ee9c076613
azhar-azad <azhar.u.azad@gmail.com>
2021-06-06 18:31:48 +0600

    e-shop: api req logger added

--- udemy/e-shop/backend/server.js
+++ udemy/e-shop/backend/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-app.get('/', (req, res) => {
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+require('dotenv/config');
+const host = process.env.HOST;
+const port = process.env.PORT;
+const api = process.env.API;
+const api_base_url = `http://${host}:${port}${api}`;
+
+app.get(`${api}/`, (req, res) => {
   res.send('hello API!');
 });
 
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: 'Hair Dresser',
+    image: 'some_image_url'
+  };
+  res.send(product);
+});
 
-app.listen(5000, () => {
-  console.log(`Server is running on http://localhost:5000`);
+app.listen(port, () => {
+  console.log(`API Server is running on ${api_base_url}`);
 });
