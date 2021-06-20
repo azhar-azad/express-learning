14e20acfa51675cc84877df9582eb6e797013af8
azhar-azad <azhar.u.azad@gmail.com>
2021-06-20 22:21:08 +0600

    devcamper: Custom Error Handler middleware added

--- udemy/devcamper-api/server.js
+++ udemy/devcamper-api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middlewares/error.middleware');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -25,6 +26,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use(`/api/v1/bootcamps`, bootcampsRouter);
+
+app.use(errorHandler); // as this handles error in controller methods, it needs to be added after the router call
 
 const PORT = process.env.PORT || 5000;
 
