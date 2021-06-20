edec2750b92d0b773e6e69af0e659956b96eb3a7
azhar-azad <azhar.u.azad@gmail.com>
2021-06-20 21:51:55 +0600

    devcamper: Fetching Bootcamp(s) GET

--- udemy/devcamper-api/controllers/bootcamps.controller.js
+++ udemy/devcamper-api/controllers/bootcamps.controller.js
@@ -6,11 +6,20 @@ const Bootcamp = require('../models/Bootcamp');
  * @route         /api/v1/bootcamps
  * @access        Public
 */
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ 
-    success: true, 
-    msg: `Show all bootcamps`
-  });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({
+      success: true,
+      data: bootcamps
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err
+    });
+  }
 };
 
 /** 
@@ -19,11 +28,26 @@ exports.getBootcamps = (req, res, next) => {
  * @route         /api/v1/bootcamps/:id
  * @access        Public
 */
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ 
-    success: true, 
-    msg: `Get bootcamp ${req.params.id}`
-  });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      error: err
+    });
+  }
 };
 
 /** 
