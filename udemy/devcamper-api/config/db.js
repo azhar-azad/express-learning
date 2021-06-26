a46cbb2c281768c2b4c417e879a41db8fc871fe9
azhar-azad <azhar.u.azad@gmail.com>
2021-06-26 23:41:41 +0600

    devcamper: seeder implemented to import data to db from JSON

--- udemy/devcamper-api/config/db.js
+++ udemy/devcamper-api/config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-// const conn_string = 'mongodb://localhost/devcamper_db';
+const conn_string = 'mongodb://localhost/devcamper_db';
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(conn_string, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
---
+++ udemy/devcamper-api/utils/seeder.js
@@ -0,0 +1,62 @@
+/** 
+ * Run command: node utils/seeder.js -i
+ * -i: to import data 
+ * -d: to delete data
+*/
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env vars
+// config file path from root
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('../models/Bootcamp');
+
+// To import in local mongodb
+// const conn_string = 'mongodb://localhost/devcamper_db';
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/../_data/bootcamps.json`, 'utf-8')
+);
+
+// Import info DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data Imported ...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('Data Destroyed ...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') { // import
+  importData();
+} else if (process.argv[2] === '-d') { // delete
+  deleteData();
+}
