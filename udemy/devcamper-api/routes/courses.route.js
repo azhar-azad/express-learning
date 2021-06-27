03b02c209c0f2bcc386ffd486241f0f6e880a175
azhar-azad <azhar.u.azad@gmail.com>
2021-06-27 18:52:35 +0600

    devcamper: Create course for bootcamp

--- udemy/devcamper-api/routes/courses.route.js
+++ udemy/devcamper-api/routes/courses.route.js
@@ -1,10 +1,15 @@
 const { 
-  getCourses
+  getCourses,
+  getCourse,
+  createCourse
 } = require('../controllers/courses.controller');
 
 // mergeParams is set to true in order to find the params from calling router
 const router = require('express').Router({ mergeParams: true }); 
 
-router.route('/').get(getCourses);
+router.route('/')
+  .get(getCourses)
+  .post(createCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
