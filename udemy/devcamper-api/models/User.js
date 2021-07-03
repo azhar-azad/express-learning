5a6c2d2f9824180178f97962d0a29776319fef03
azhar-azad <azhar.u.azad@gmail.com>
2021-07-03 14:50:37 +0600

    devcamper: Sign & get json web token

--- udemy/devcamper-api/models/User.js
+++ udemy/devcamper-api/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -41,5 +42,15 @@ UserSchema.pre('save', async function(next) {
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
 });
+
+// Sign JWT and return
+// It is a mongoose method that actually being called on the 'user' data, not on the model
+// So, this._id refers to the user._id
+// Also, this method can be called form controller
+UserSchema.methods.getSignedJwtToken = function() {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE
+  });
+}
 
 module.exports = mongoose.model('User', UserSchema);
