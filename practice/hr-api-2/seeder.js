cec589fc3863aa62448e74286eefce4d41e71492
azhar-azad <azhar.u.azad@gmail.com>
2021-07-04 19:34:16 +0600

    hr2: seeder implemented

---
+++ practice/hr-api-2/seeder.js
@@ -0,0 +1,59 @@
+/** 
+ * Run command: node seeder.js -i
+ * -i: to import data 
+ * -d: to delete data
+*/
+
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+// Load env vars
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Employee = require('./models/Employee');
+
+// Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const employees = JSON.parse(
+  fs.readFileSync(`${__dirname}/_mock_data/employees.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Employee.create(employees);
+
+    console.log('Data Imported ... '.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Employee.deleteMany();
+
+    console.log('Data Destroyed ... '.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-i') { // import
+  importData();
+} else if (process.argv[2] === '-d') { // delete
+  deleteData();
+}
